7ff47c635aa79646cf6531e114b2e9f7970c7780
murugeshhiremat7 <112333638+murugeshhiremat7@users.noreply.github.com>
2022-10-10 20:46:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ docker stop project2 || true;
 docker rm -f project2 || true;
 docker image rm -f project2 || true;
 docker build -t project2 .;
-docker run -dit --name project2 -p 80:80 html2_vinod;
+docker run -dit --name project2 -p 80:80 project2;
 docker login -u vnom1985 -p abc@12345;
 docker tag project2 vnom1985/project2;
 docker push vnom1985/project2
